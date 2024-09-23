5b84ae635be1b78576f0afe51b3b34b30ac6a651
RJayBernales <rainieljevson0@gmail.com>
2024-09-23 20:50:52 +0800

    Added a Description/Comment to Define Which is Which

--- it20c/src/Multidim/Multidimensional_Array.java
+++ it20c/src/Multidim/Multidimensional_Array.java
@@ -2,15 +2,16 @@
 package Multidim;
 
  
-public class Multidimentional_Array {
+public class Multidimensional_Array {
     public static void main(String[] args) {
-        
+            //inputs for the array(in my case I designed a Simple Character)
             String[][] arrays = {
                 {".", "-", "-", "-", "."},
                 {"|", "0", ",", "0", "|", " ", "?"},
                 {"[", " ", "-", " ", "]"},
                 {"|", "_", "|", "_", "|"}
             };
+            //a nested for loop to make the array multdimensional 
             for(int i=0; i < arrays.length; i++) {
                 for(int j=0; j<arrays[i].length;j++) {
             System.out.print(arrays[i][j]);
