8c3b0ea9ea54c94ff0315c4d41baa2c54434095b
RJayBernales <rainieljevson0@gmail.com>
2024-09-23 20:28:22 +0800

    Added Description For The Simple Implementation

--- it20c/src/S_I/Simple_Implementation.java
+++ it20c/src/S_I/Simple_Implementation.java
@@ -3,10 +3,17 @@ package S_I;
 
 public class Simple_Implementation {
         public static void main(String[] args) {
+            //Added a list of array for input
             String[] Color ={"Red", "Blue", "Yellow", "Black"};
             
+            //Added a process & output using S.O.P.
             System.out.println(Color[1]);
             System.out.println(Color[0]);
             System.out.println(Color[2]);
         }
 }
+            /* The Output should be
+                Blue
+                Red
+                Yellow
+            */
