44d765bf8cd0ebe7153da74d340df671d5a4b5fd
RJayBernales <rainieljevson0@gmail.com>
2024-09-23 20:01:11 +0800

    Initial Commit with a java file that outputs the subject name

---
+++ it20c/src/it20c/It20c.java
@@ -0,0 +1,16 @@
+
+package it20c;
+
+public class It20c {
+
+    public static void main(String[] args) {
+        
+        //inputs 
+        String[] Intro ={"Welcome to", " Data Structures & Algorithm"};
+        //Adding print statement to call the array(Process)
+        System.out.print(Intro[0]);
+        System.out.print(Intro[1]);
+    
+    }
+    
+}
