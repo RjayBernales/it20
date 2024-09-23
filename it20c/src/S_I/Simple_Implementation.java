1f7ad52dd6dc257c39581d06cabe0a5c25f0aa93
RJayBernales <rainieljevson0@gmail.com>
2024-09-23 20:18:34 +0800

    Added a simple array implementation

---
+++ it20c/src/S_I/Simple_Implementation.java
@@ -0,0 +1,12 @@
+
+package S_I;
+
+public class Simple_Implementation {
+        public static void main(String[] args) {
+            String[] Color ={"Red", "Blue", "Yellow", "Black"};
+            
+            System.out.println(Color[1]);
+            System.out.println(Color[0]);
+            System.out.println(Color[2]);
+        }
+}
