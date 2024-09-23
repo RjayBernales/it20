a692c3af6a1b8bb5c72740547cb4a555bff5092b
RJayBernales <rainieljevson0@gmail.com>
2024-09-23 20:32:51 +0800

    Added an Example of Array Displayed Using Loops

---
+++ it20c/src/Looping/Display_Using_Loop.java
@@ -0,0 +1,16 @@
+ 
+package Looping;
+
+ 
+public class Display_Using_Loop {
+    public static void main(String[] args) {
+   
+        String[] Animechar ={"Shadow", "Alpha", "Beta", "Delta"};
+        
+        for (int i = 0; i<Animechar.length; i++){
+            System.out.println(Animechar[i]);
+        }
+        
+        
+    }
+}
