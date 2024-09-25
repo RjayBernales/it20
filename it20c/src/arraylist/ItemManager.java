a7da1294afe0c05c66a80d60644ed4bf76a3e3dd
RJayBernales <rainieljevson0@gmail.com>
2024-09-25 20:52:55 +0800

    Added an arraylist that had a function so that the user can add a name to the arraylist

---
+++ it20c/src/arraylist/ItemManager.java
@@ -0,0 +1,24 @@
+//Adding package for arraylist
+
+package arraylist;
+
+import java.util.ArrayList;
+
+
+public class ItemManager {
+        
+        private ArrayList<String> items;
+        
+        public ItemManager(){
+            items = new ArrayList<>();
+        }
+        //Added function to display the items
+        public void displayItems(){
+            System.out.println("Names on our list: " +items);
+        }
+        //Added a function to add items into the arraylist
+        public void addName(String item){
+            items.add(item);
+            System.out.println("Names: " + items);
+        } 
+}
---
+++ it20c/src/arraylist/Main.java
@@ -0,0 +1,27 @@
+package arraylist;
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args){
+        
+        //Added a scanner to have user inputs
+        Scanner sc = new Scanner(System.in);
+        
+        //add a default list
+        ItemManager list = new ItemManager();
+        list.addName("Jack");
+        
+        
+        //displaying the updated items
+        list.displayItems();
+        
+        //Added a println that corresponds with the codes on ItemManager.java
+        System.out.println("Enter the Name You Want to Add: ");
+        String item = sc.nextLine();
+        
+        //adding item
+        list.addName(item);
+    }
+}
