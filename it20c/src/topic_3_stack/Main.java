08acb3f3ae830308eada07a9eb0515a884c314a6
RJayBernales <rainieljevson0@gmail.com>
2024-10-11 22:31:32 +0800

    Added cases to swtch for the user to check the size, add in it, delete, and exit the program

--- it20c/src/topic_3_stack/Main.java
+++ it20c/src/topic_3_stack/Main.java
@@ -12,7 +12,7 @@ public class Main {
     
         Stack stack = new Stack(5);
     
-        System.out.println("~~~~~~~~~~~~~~~~~~~");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         
          while(true) {
             System.out.println("What Do You Want: "
@@ -22,8 +22,31 @@ public class Main {
                     + " 4. Exit");
             int choice = sc.nextInt();
             switch(choice) {
-                
+                case 1:
+                      System.out.println("Stack size is: " + stack.size());
+                      System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                      break;
+                case 2:   
+                     System.out.println("Add a number: ");
+                     int num = sc.nextInt();
+                     System.out.println("Pushed value: " + num);
+                     stack.push(num);
+                     stack.size();
+                     System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                     break;    
+                case 3:                   
+                     stack.peek();
+                     System.out.println("Popped element: " + stack.pop());
+                     stack.size();
+                     break;   
+                case 4:
+                     System.out.println("Thank You For Using The Program");
+                     System.exit(0);
+                     break;
+                 default:
+                        System.out.println("Invalid choice. Please select a valid option.");    
+        
+                }
             }
-       }
- }
+    }
 }
