2616501fb52fcc549f51a09d0b269bb99f8d2bf3
RJayBernales <rainieljevson0@gmail.com>
2024-10-11 21:43:49 +0800

    Added a new method size

--- it20c/src/topic_3_stack/Stack.java
+++ it20c/src/topic_3_stack/Stack.java
@@ -16,10 +16,17 @@ public class Stack {
         this.stackArray = new int[maxSize];
         this.top = -1; 
     }
+     
      public boolean isEmpty() {
         return (top == -1);
     }
+     
      public boolean isFull() {
         return (top == maxSize - 1);
     }
+     
+     public int size() {
+        return top + 1;
+    }
+     
 }
