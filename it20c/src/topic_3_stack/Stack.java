437598d591db314652bbe1fe7d5fee33641e1dbb
RJayBernales <rainieljevson0@gmail.com>
2024-10-11 22:29:07 +0800

    Added some changes to Stack.java

--- it20c/src/topic_3_stack/Stack.java
+++ it20c/src/topic_3_stack/Stack.java
@@ -10,25 +10,20 @@ public class Stack {
     private int[] stackArray;
     private int top;
     
- 
      public Stack(int size) {
         this.maxSize = size;
         this.stackArray = new int[maxSize];
         this.top = -1; 
-    }
-     
+    } 
      public boolean isEmpty() {
         return (top == -1);
     }
-     
      public boolean isFull() {
         return (top == maxSize - 1);
     }
-     
      public int size() {
         return top + 1;
     }
-     
      public void push(int value) {
         if (isFull()) {
             System.out.println("Stack is full. Cannot push " + value);
@@ -36,7 +31,6 @@ public class Stack {
         }
         stackArray[++top] = value;
     }
-     
      public int peek() {
         if (isEmpty()) {
             System.out.println("Stack is empty. Cannot peek.");
