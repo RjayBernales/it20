5a2523e7adc26ad9a5cf90d99ee02cf531ecd593
RJayBernales <rainieljevson0@gmail.com>
2024-10-11 21:51:50 +0800

    Added while loop o iterate the code

--- it20c/src/topic_3_stack/Main.java
+++ it20c/src/topic_3_stack/Main.java
@@ -1,15 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package topic_3_stack;
+
+import java.util.Scanner;
 
 /**
  *
- * @author Admin
+ * @author RJ
  */
 public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
     
+        Stack stack = new Stack(5);
+    
+        System.out.println("~~~~~~~~~~~~~~~~~~~");
+        
+         while(true) {
+
+       }
+ }
 }
