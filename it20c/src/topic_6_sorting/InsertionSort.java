34cbd4e857205a49e0b10ccf390ab66e770a3db6
RJayBernales <rainieljevson0@gmail.com>
2024-11-29 00:07:58 +0800

    Added main method and scanner to accept user input

--- it20c/src/topic_6_sorting/InsertionSort.java
+++ it20c/src/topic_6_sorting/InsertionSort.java
@@ -5,6 +5,8 @@
  */
 package topic_6_sorting;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
@@ -31,4 +33,11 @@ public class InsertionSort {
         }
         System.out.println();
     }
+    public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+             int[] arr = null;
+             long timeTaken = 0;
+     }
 }
