5f9192ea4cfaaad1aa8edb3fd052cb7c3e26f357
RJayBernales <rainieljevson0@gmail.com>
2024-11-29 00:11:42 +0800

    Added while statement containing the choices which is then connected to switch statement

--- it20c/src/topic_6_sorting/InsertionSort.java
+++ it20c/src/topic_6_sorting/InsertionSort.java
@@ -35,9 +35,33 @@ public class InsertionSort {
     }
     public static void main(String[] args) {
 
-            Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-             int[] arr = null;
-             long timeTaken = 0;
+        int[] arr = null;
+        long timeTaken = 0;
+             
+        while(true) {
+
+                 System.out.println("Kindly choose a number: "
+                        + " 1. Add array Elements"
+                        + " 2. View sorted array and time taken."
+                        + " 3. Exit");
+                int choice = sc.nextInt();
+
+                switch(choice) {
+                    case 1:
+                            System.out.println();
+                            System.out.println("Enter the number of elements you want to store in the array:");
+                            int num = sc.nextInt();
+                            arr = new int[num];
+
+                            System.out.println("Enter " + num + " elements.");
+                            for(int i = 0; i < num; i++) {
+                              arr[i] = sc.nextInt();
+                            }
+                            break;
+                }
+             }
      }
+    
 }
