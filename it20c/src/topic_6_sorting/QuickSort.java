14cee7d8b56110c727ea6288e0334aa6217a8161
RJayBernales <rainieljevson0@gmail.com>
2024-11-29 00:38:38 +0800

    Added a switch statement and case 1

--- it20c/src/topic_6_sorting/QuickSort.java
+++ it20c/src/topic_6_sorting/QuickSort.java
@@ -63,7 +63,21 @@ public class QuickSort {
                         + " 2. View sorted array"
                         + " 3. Exit");
                 int choice = sc.nextInt();
-                
-             }
+            switch(choice) {
+                    case 1:
+                            System.out.println();
+                            System.out.println("Enter the number of elements you want to store in the array:");
+                            int num = sc.nextInt();
+                            arr = new int[num];
+
+                            System.out.println("Enter " + num + " elements.");
+                            for(int i = 0; i < num; i++) {
+                              arr[i] = sc.nextInt();
+                            }
+                            break;
+                 }    
+             
+            }
+            
     }
 }
