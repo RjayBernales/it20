48dabb54fc7ee185da1d93361e21738d2d6f9026
RJayBernales <rainieljevson0@gmail.com>
2024-11-29 00:34:48 +0800

    Added quicksort, partition and printArray including the main method which contains the scanner and the array var

--- it20c/src/topic_6_sorting/QuickSort.java
+++ it20c/src/topic_6_sorting/QuickSort.java
@@ -5,10 +5,55 @@
  */
 package topic_6_sorting;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
  */
 public class QuickSort {
-    
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+             int[] arr = null;
+             long timeTaken = 0;
+    }
 }
