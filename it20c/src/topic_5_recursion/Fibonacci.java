01f0fd1cdb8b40a34b6d227d3fe14cd7a256e1ec
RJayBernales <rainieljevson0@gmail.com>
2024-12-02 22:23:39 +0800

    Added a method which computes the fibonacci's number at position using recursive method

--- it20c/src/topic_5_recursion/Fibonacci.java
+++ it20c/src/topic_5_recursion/Fibonacci.java
@@ -11,5 +11,12 @@ package topic_5_recursion;
  * @author Admin
  */
 public class Fibonacci {
-    
+    public int fibonacci(int n) {
+		 // Base cases
+	     if (n == 0) return 0;
+	     if (n == 1) return 1;
+
+	     // Recursive case
+	     return fibonacci(n - 1) + fibonacci(n - 2);
+	  }
 }
