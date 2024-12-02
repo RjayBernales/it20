c6c2061698a8416e214713df9d31998c5bd01801
RJayBernales <rainieljevson0@gmail.com>
2024-12-02 22:27:05 +0800

    Added a code whch calls the recursive fibunacci method and display the fibonacci's computed number

--- it20c/src/topic_5_recursion/Fibonacci.java
+++ it20c/src/topic_5_recursion/Fibonacci.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
  */
 public class Fibonacci {
     public int fibonacci(int n) {
-		 // Base cases
+             // Base cases
 	     if (n == 0) return 0;
 	     if (n == 1) return 1;
 
@@ -24,5 +24,14 @@ public class Fibonacci {
     public static void main(String[] args) {
 	     
 		 Scanner sc = new Scanner(System.in);
+                 Fibonacci calculator = new Fibonacci();
+
+		 System.out.print("Enter a number to calculate its factorial: ");
+                 int number = sc.nextInt(); 
+	     
+		 int result = calculator.fibonacci(number);
+		 System.out.println("Fibonacci number at position " + number + " is: " + result);
+	        
+		 sc.close();
     }
 }
